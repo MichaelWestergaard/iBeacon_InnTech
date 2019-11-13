5ef920c99d89dd72c9ff70ed4e5094e8d4f1df36
Michael Westergaard - Laptop <michaelwestergaard@hotmail.dk>
2019-11-13 11:21:12 +0100

    start skærm

--- lib/main.dart
+++ lib/main.dart
@@ -66,46 +66,43 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Stack(
+        children: <Widget>[
+          Image(image: AssetImage("assets/images/background.jpg"),fit: BoxFit.fill,),
+          Positioned(
+            bottom: 90,
+            left: 4,
+            child: InkWell(
+              onTap: () => print("new screen"),
+              child: Card(
+                shape: BeveledRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(1))),
+                elevation: 6,
+                child: Container(
+                  width: MediaQuery.of(context).size.width/2-10,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                    child: Column(
+                      children: <Widget>[
+                        Icon(
+                          Icons.directions,
+                          size: 50,
+                          color: Color(0xff0ab277),
+                        ),
+                        Text(
+                          "Planlægning",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w900
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          )
+        ],
+      )
     );
   }
 }
