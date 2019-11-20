4c5a0b61de33ea67249f9c89dca737e05f023e4d
Mikkel Berg Eriksen <eriksen127@msn.com>
2019-11-20 09:00:52 +0100

    Plan.dart

--- lib/Plan.dart
+++ lib/Plan.dart
@@ -8,21 +8,52 @@ class Plan extends StatefulWidget {
 class _PlanState extends State<Plan> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        RaisedButton(
-          child: Text("Cardio"),
-        ),
-        RaisedButton(
-          child: Text("Styrketræning"),
-        ),
-        RaisedButton(
-          child: Text("Holdtræning"),
-        ),
-        RaisedButton(
-          child: Text("Faciliteter"),
-        ),
-      ],
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 75,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
+              child: RaisedButton(
+                child: Text("Cardio"),
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 75,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
+              child: RaisedButton(
+                child: Text("Styrketræning"),
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 75,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
+              child: RaisedButton(
+                child: Text("Holdtræning"),
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 75,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
+              child: RaisedButton(
+                child: Text("Faciliteter"),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
